da58a25c9804855403bf44778458789437932a85
kerry <813711479@qq.com>
2018-10-23 09:30:22 +0800

    删除无关代码

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const Koa = require('koa');
 const path = require('path');
 const views = require('koa-views');
-const content = require('./utils/content');
-const mimes = require('./utils/mime');
 const app = new Koa();
-const static = "./public";
+const static = require('koa-static')
+//设置静态资源的路径
+const staticPath = './public';
 const bodyParser = require('koa-bodyparser');
 const route = require('./router/route');
 const session = require('koa-session');
-const jwt = require('koa-jwt');
+// const jwt = require('koa-jwt');
 const config = require('./config/default');
 
 // app.use(jwt({ secret: config.secret }));
@@ -25,33 +25,9 @@ app.use(views(path.join(__dirname, './views'), {extensions: 'html'}));
 
 app.use(route);
 
+app.use(static(
+    path.join( __dirname,  staticPath)
+));
 
 
-// 解析资源类型
-function parseMime( url ) {
-    let extName = path.extname( url );
-    extName = extName ?  extName.slice(1) : 'unknown';
-    return  mimes[ extName ]
-}
-
-app.use( async (ctx)=>{
-   let fullPath = path.join(__dirname, static);
-   let _content = await content( ctx, fullPath );
-
-    // 解析请求内容的类型
-    let _mime = parseMime( ctx.url );
-    if(_mime){
-        ctx.type = _mime;
-    }
-    if(_mime && _mime.indexOf("image/")>=0){
-        ctx.res.writeHead(200);
-        ctx.res.write(_content, 'binary');
-        ctx.res.end()
-    }else {
-        // 其他则输出文本
-        ctx.body = _content
-    }
-
-
-});
 app.listen(3000);
